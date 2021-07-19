53f2a8d783ab7f9f014924309b395fce192db3a1
unknown <faotqm@hotmail.com>
2021-07-19 15:08:01 -0500

    Implementacion de Vistas

--- src/Libreries/Core/Views.php
+++ src/Libreries/Core/Views.php
@@ -3,7 +3,7 @@
 class Views{
     function getView($controller, $view){
         $controller = get_class($controller);
-        if ($controller == "Home") {
+        if ($controller == "HomeController") {
             $view = 'views/' . $view . ".php";
         } else {
             $view = 'views/' . $controller . "/" . $view . ".php";
