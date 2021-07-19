9c7b4c7239715bc5322695e2e732d4ef4500f844
unknown <faotqm@hotmail.com>
2021-07-19 18:47:23 -0500

    Funciones de bases de datos implementadas

--- src/Config/config.php
+++ src/Config/config.php
@@ -1,15 +1,15 @@
 <?php
 date_default_timezone_set('America/Mexico_City');
-const PRODUCCION = false;
+const PRODUCCION = true;
 const LIBS = "src/Libreries/Core/";
 const VIEWS = "Views";
 
 if (PRODUCCION) {
     define('BASE_URL', "");
-    define('DB_HOST', "");
-    define('DB_NAME', "");
-    define('DB_USER', "");
-    define('DB_PASSWORD', "");
+    define('DB_HOST', "localhost");
+    define('DB_NAME', "u784873018_proyect_mvc");
+    define('DB_USER', "u784873018_hotaruV");
+    define('DB_PASSWORD', "Lu~d?>d2&");
     define('DB_CHARSET', "utf8");
 }else{
     define('BASE_URL', "http://localhost/drops__framework/original_framework/");
