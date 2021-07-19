af85eccb3cb0d2543436ed08142755fc24615e01
unknown <faotqm@hotmail.com>
2021-07-19 14:50:25 -0500

    probando modelos

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -10,5 +10,9 @@ class Home extends Controllers{
     public function saludar($params){
         echo "Saludo numero: ". $params;
     }
+    public function carrito($params){
+        $carrito = $this->model->getCarrito($params);
+        echo $carrito;
+    }
     
 }
--- app/Models/HomeModel.php
+++ app/Models/HomeModel.php
@@ -2,6 +2,9 @@
 class homeModel{
     public function __construct(){
         echo "a webo estoy obteniendo un mensaje desde el modelo <br>";
-        echo "Aleluya";
+        echo "Aleluya<br>";
+    }
+    public function getCarrito($params){
+        return "Datos del Carrito no ". $params;
     }
 }
