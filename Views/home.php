50c3d540baff22300a1a14616c5ef94d049ec2d7
unknown <faotqm@hotmail.com>
2021-07-20 18:23:24 -0500

    configuracion de login en javascript

--- Views/home.php
+++ Views/home.php
@@ -5,16 +5,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php require_once 'estilos.php' ?>
     <title><?= $data['tag_pages'] ?></title>
+    <?php require_once('estilos.php'); ?>
 </head>
 
 <body>
     <h1 style="font-size: 4em;"><?= $data['pages_title'] ?></h1>
     <p style="font-size: 2em;"><?= $data['pages_name'] ?></p>
-    
-    
-    <?php require_once 'scripts.php' ?>
+
 </body>
+<?php require_once 'scripts.php' ?>
 
 </html>
--- public/js/login_functions.js
+++ public/js/login_functions.js
@@ -8,32 +8,34 @@ function cargando() {
             e.preventDefault();
             let strEmail = document.querySelector('#loginEmail').value;
             let strPass = document.querySelector('#loginPassword').value;
-            
+
             if (strEmail == '' || strPass == '') {
                 alert('los campos estan vacios')
             } else {
                 let data = new FormData(formLogin);
-               
-                
+
+
                 let link = base_url + "/login/ingresar"
                 fetch(link, {
                     method: 'POST',
-                    body: data,   
+                    body: data,
                 })
-                .then(function(response) {
-                    if(response.ok) {
-                        return response.text()
-                    } else {
-                        throw "Error en la llamada Ajax";
-                    }
-                 
-                 })
-                 .then(function(texto) {
-                    console.log(texto);
-                 })
-                 .catch(function(err) {
-                    console.log(err);
-                 });
+                    .then(function (response) {
+                        if (response.ok) {
+                            return response.json()
+                        } else {
+                            throw "Error en la llamada Ajax";
+                        }
+
+                    })
+                    .then(function (res) {
+                        if(res.msg=='ok'){
+                            location.href = base_url+'/home/home'
+                        }
+                    })
+                    .catch(function (err) {
+                        console.log(err);
+                    });
             }
         }
     }
