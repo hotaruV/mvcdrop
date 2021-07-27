05e8708923d4b25b0c33d63badd18d1fa0e07cbc
unknown <faotqm@hotmail.com>
2021-07-27 10:24:13 -0500

    Correccion en errores

--- src/Config/config.php
+++ src/Config/config.php
@@ -1,15 +1,15 @@
 <?php
 date_default_timezone_set('America/Mexico_City');
-const PRODUCCION = true;
+const PRODUCCION = false;
 const LIBS = "src/Libreries/Core/";
 const VIEWS = "Views";
 
 if (PRODUCCION) {
-    define('BASE_URL', "http://mvcdrop.hospitalolga.com");
+    define('BASE_URL', "https://fly-select.com/vuelos-al-caribe");
     define('DB_HOST', "localhost");
-    define('DB_NAME', "u784873018_proyect_mvc");
-    define('DB_USER', "u784873018_hotaruV");
-    define('DB_PASSWORD', "Lu~d?>d2&");
+    define('DB_NAME', "flyselec_proyect_mvc");
+    define('DB_USER', "flyselec_hotaruv");
+    define('DB_PASSWORD', "LCzY60+]vsb6");
     define('DB_CHARSET', "utf8");
 }else{
     define('BASE_URL', "http://localhost/drop");
